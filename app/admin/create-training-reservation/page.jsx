5a02bff0d4beb6d154a3a827c1edd21a8031cf7d
Iveta Šinálová <iveta.sinalova78@gmail.com>
2026-02-07 16:33:25 +0100

    vycvik s ubytovanim added

--- app/admin/create-training-reservation/page.jsx
+++ app/admin/create-training-reservation/page.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import AccommodationCalendar from "../../../components/AccommodationCalendar";
 
 function page() {
-  return <AccommodationCalendar serviceName={"Výcvik psa"} />;
+  return <AccommodationCalendar serviceName={"Výcvik s ubytovaním"} />;
 }
 
 export default page;
